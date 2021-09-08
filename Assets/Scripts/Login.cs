e3539298a9ebfdbb413f7cd9938606ae4f2ba6dc
Tomás Ferreyra <tomaslicenciado@gmail.com>
2021-09-08 13:15:22 -0300

    Agregando Login simple

---
+++ Assets/Scripts/Login.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Login : MonoBehaviour
+{
+    public InputField inputUsername;
+    public InputField inputPassword;
+    public Text textLoginResult;
+    public string escena;
+
+
+    public void login()
+    {
+        if ((inputUsername.text == "admin" && inputPassword.text == "admin")||(inputUsername.text == "usuario" && inputPassword.text == "usuario")){
+            textLoginResult.text = "Login correcto";
+            SceneManager.LoadScene(escena);
+        }
+        else {
+            textLoginResult.text = "Login incorrecto";
+        }
+    }
+
+}
