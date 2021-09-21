01fb09915086454ba28731cc244705ff394232d9
Tomás Ferreyra <tomaslicenciado@gmail.com>
2021-09-21 20:34:11 -0300

    Login with bd on AWS

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -12,16 +12,54 @@ public class Login : MonoBehaviour
     public Text textLoginResult;
     public string escena;
 
+    IEnumerator ieLogin()
+    {
+        WWWForm dataForm = new WWWForm();
+        dataForm.AddField("Username", inputUsername.text);
+        dataForm.AddField("Password", inputPassword.text);
+
+        string uri = "http://ec2-52-91-89-222.compute-1.amazonaws.com/login/login.php";
+
+        UnityWebRequest webRequest = UnityWebRequest.Post(uri, dataForm);
+        webRequest.chunkedTransfer = false;
+
+        yield return webRequest.SendWebRequest();
+
+
+        string result = webRequest.downloadHandler.text;
+
+
+        if ((result != "0")&&(result != "Connection Error!"))
+        {
+            textLoginResult.text = "Sign in successfully !";
+            PlayerPrefs.SetString("user_name", result);
+            SceneManager.LoadScene(escena);
+        }
+
+        else if( result == "0")
+        {
+            textLoginResult.text = "You have to register !";
+        }
+
+
+        else
+        {
+            textLoginResult.text = "Check your connection !";
+        }
+
+
+
+    }
 
     public void login()
     {
-        if ((inputUsername.text == "admin" && inputPassword.text == "admin")||(inputUsername.text == "usuario" && inputPassword.text == "usuario")){
-            textLoginResult.text = "Login correcto";
-            SceneManager.LoadScene(escena);
-        }
-        else {
-            textLoginResult.text = "Login incorrecto";
-        }
+        StartCoroutine(ieLogin());
+        // if ((inputUsername.text == "admin" && inputPassword.text == "admin")||(inputUsername.text == "usuario" && inputPassword.text == "usuario")){
+        //     textLoginResult.text = "Login correcto";
+        //     SceneManager.LoadScene(escena);
+        // }
+        // else {
+        //     textLoginResult.text = "Login incorrecto";
+        // }
     }
-
 }
